096917200db6dd1ed0c0b672d368c940babe17ac
Herczeg István <wpe04q@inf.elte.hu>
2021-01-06 11:18:57 +0100

    Upload New File

---
+++ experts-hi/src/main/java/p4analyser/ExpertHI.java
@@ -0,0 +1,68 @@
+package p4analyser;
+
+public class ExpertHI 
+{
+    public static void main( String[] args )
+    {
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        String remoteTraversalSourceName =args[2];
+
+        GraphTraversalSource g = AnonymousTraversalSource.traversal()
+            .withRemote(DriverRemoteConnection.using(host, port, remoteTraversalSourceName));
+        
+        analyse(g);
+
+        System.out.println("OK");
+    }
+
+    public static void analyse(GraphTraversalSource g)
+    {
+        List<Object> assignmentsLeft = new ArrayList<Object>;
+        List<Object> assignmentsRight = new ArrayList<Object>;
+
+        // Queries
+        assignmentsleft = g.V().out().has('Dom.Syn.V.CLASS', "AssignMentOrMethodCallStatementContext")
+            outE(Dom.Syn).has(Dom.Syn.E.RULE, "lvalue")
+            .inV().has(Dom.Syn.V.VALUE, "dstAddr")
+            .repeat(.out(Dom.SYN))
+            .until(.has(Dom.Syn.V.CLASS, "TerminalNodeImpl"))
+            .values(Dom.Syn.V.LINE).toList();
+        
+        assignmentsRight = g.V().out().has('Dom.Syn.V.CLASS', "AssignMentOrMethodCallStatementContext")
+            .outE(Dom.Syn).has(Dom.Syn.E.RULE, "expression")
+            .inV().has(Dom.Syn.V.VALUE, "dstAddr")
+            .repeat(.out(Dom.SYN))
+            .until(.has(Dom.Syn.V.CLASS, "TerminalNodeImpl"))
+            .values(Dom.Syn.V.LINE).toList();
+
+        // Left assignments output
+        if(assignmentsLeft.size() > 0)
+        {
+            for(int i=0; i<nodes.size(); i++)
+            {
+                System.out.println("Left assignment " + i+1 + ": " + assignmentsLeft.get(i));
+            }
+        }
+        else
+        {
+            System.out.println("No left assignment found.");
+        }
+
+        // Right assignments output
+        if(assignmentsRight.size() > 0)
+        {
+            for(int i=0; i<nodes.size(); i++)
+            {
+                System.out.println("Right assignment " + i+1 + ": " + assignmentsRight.get(i));
+            }
+        }
+        else
+        {
+            System.out.println("No right assignment found.");
+        }
+
+        // Completed
+        System.out.println("Task completed.");
+    }
+}
