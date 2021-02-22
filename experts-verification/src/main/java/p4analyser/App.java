eae1d36995ae24545e6685e4c55a3eb014847d9f
tothgabi <kistoth@inf.elte.hu>
2021-02-22 10:27:23 +0100

    add expert-verification

---
+++ experts-verification/src/main/java/p4analyser/App.java
@@ -0,0 +1,13 @@
+package p4analyser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
