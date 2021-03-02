6c16c9d11f47ecb967084c4125f5a4bac0c7e14c
Hegyi Péter <immsrb@inf.elte.hu>
2021-03-02 16:44:55 +0100

    update fejtoro solution

--- experts-fejtoro-hp/src/main/java/p4analyser/FejtoroHp.java
+++ experts-fejtoro-hp/src/main/java/p4analyser/FejtoroHp.java
@@ -1,5 +1,6 @@
 package p4analyser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
@@ -7,6 +8,7 @@ import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.structure.Property;
+import org.apache.tinkerpop.shaded.jackson.annotation.JsonTypeInfo.Id;
 
 import p4analyser.ontology.Dom;
 
@@ -20,24 +22,51 @@ public class FejtoroHp {
         GraphTraversalSource g = AnonymousTraversalSource.traversal()
                 .withRemote(DriverRemoteConnection.using(host, port, remoteTraversalSourceName));
 
-        System.out.println(analyse_2(g));
-
-        System.out.println("OK");
+        //List<Object> list = analyse_2(g, "ipv4");
+        //System.out.println(analyse(g, "ipv4"));
+        List<Object> list = analyse(g, "ipv4");
+        System.out.println(getPropertyFromList(Dom.Syn.V.LINE, list));
+        System.out.println(getControl(g, getPropertyFromList(Dom.Syn.V.NODE_ID, list)));
     }
 
-    public static List<? extends Property<Object>> analyse_2(GraphTraversalSource g) {
+    public static List<Object> analyse(GraphTraversalSource g, String headerName) {
         return g.V().has(Dom.Syn.V.CLASS,"ConditionalStatementContext").and(
                 __.out().has(Dom.Syn.V.VALUE, "if"),
                 __.repeat(__.out()).until(__.has(Dom.Syn.V.CLASS, "ExpressionContext")).out().and(
                     __.repeat(__.out()).until(__.has(Dom.Syn.V.CLASS, "ExpressionContext")).and(
                         __.repeat(__.out()).until(__.has(Dom.Syn.V.VALUE, "hdr")),
                         __.out().has(Dom.Syn.V.VALUE, "."),
-                        __.repeat(__.out()).until(__.has(Dom.Syn.V.VALUE, "ipv4"))
+                        __.repeat(__.out()).until(__.has(Dom.Syn.V.VALUE, headerName))
                     ),
                     __.out().has(Dom.Syn.V.VALUE, "."),
                     __.repeat(__.out()).until(__.has(Dom.Syn.V.VALUE, "isValid"))
                 )
-        ).properties(Dom.Syn.V.LINE).toList();
+        ).properties(Dom.Syn.V.NODE_ID, Dom.Syn.V.LINE).value().toList();
+    }
+
+    public static List<Object> getControl(GraphTraversalSource g, List<Object> nodeIds) {
+        List<Object> result = new ArrayList<Object>();
+        for(int i = 0; i < nodeIds.size(); i++){
+            List<Object> subresult = g.V().has(Dom.Syn.V.NODE_ID, nodeIds.get(i)).repeat(__.in()).until(
+                __.has(Dom.Syn.V.CLASS, "ControlDeclarationContext")).out().has(Dom.Syn.V.CLASS,"ControlTypeDeclarationContext")
+                    .out().has(Dom.Syn.V.CLASS, "NameContext").repeat(__.out()).times(3)
+                    .properties(Dom.Syn.V.VALUE).value().unfold().toList();
+        
+            result.addAll(subresult);
+        }
+
+        return result;
+    }
+
+    public static List<Object> getPropertyFromList(String prop, List<Object> propertyList) {
+        List<Object> ret = new ArrayList<Object>();
+        int mod = prop == Dom.Syn.V.LINE ? 0 : 1;
+        for(int i = 0; i < propertyList.size(); i++) {
+            if(i % 2 == mod) {
+                ret.add(propertyList.get(i));
+            }
+        }
+        return ret;
     }
     
 }
